f2884453d69b5c94b5c65cf8f58d05ada8e82d82
HyunnoN <hyunnoh01@gmail.com>
2020-07-25 00:40:20 +0000

    Add setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-git config --global user.name "HyunnoN"
+git config --global user.name "HyunnoH"
 git config --global user.email "hyunnoh01@gmail.com"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+localectl set-x11-keymap kr pc105 kr104 korean:ralt_hangul
